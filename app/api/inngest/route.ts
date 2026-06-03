d4aa19357bc87810b3d8ddc6205a5500e864fe3d
Hasan <a.aricihasann@gmail.com>
2026-06-03 19:11:29 +0300

    Set Inngest route runtime to nodejs

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { pdfTranslateFunction } from "@/lib/inngest/functions/pdfTranslate";
 
+export const runtime = "nodejs";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [pdfTranslateFunction],
