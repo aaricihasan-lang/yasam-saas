c915d8291c6c33412ce5f1f09fe4db75604833b5
Hasan <a.aricihasann@gmail.com>
2026-06-12 22:17:31 +0300

    Hide import notes in stone knowledge articles

--- app/dogaltas/tas-bilgi-kutuphanesi/page.tsx
+++ app/dogaltas/tas-bilgi-kutuphanesi/page.tsx
@@ -704,13 +704,6 @@ export default function TasBilgiKutuphanesiPage() {
                       {renderContent(selectedRecord.icerik, searchTerms)}
                     </div>
 
-                    {/* Alt not */}
-                    {selectedRecord.notlar && (
-                      <div className="mt-10 rounded-xl border border-amber-200 bg-amber-50 px-5 py-4 text-sm font-medium text-amber-800">
-                        <span className="font-black">📌 Not: </span>
-                        {selectedRecord.notlar}
-                      </div>
-                    )}
                   </article>
                 </div>
               </div>
